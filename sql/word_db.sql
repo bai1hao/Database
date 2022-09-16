28b81268b64a64d4504d66efb67e8635de1e7ca1
bai1hao <bai1hao@outlook.com>
2022-09-17 01:53:31 +0800

    修改单词本创建时间为CURRENT_TIMESTAMP自动创建 修改单词本type列为name列

--- sql/word_db.sql
+++ sql/word_db.sql
@@ -12,8 +12,8 @@ create table `user` (
 create table `book` (
     `id` bigint unsigned not null auto_increment,
     `user_id` bigint unsigned not null,
-    `create_time` varchar(255) not null, 
-    `type` varchar(50) not null,
+    `create_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',  
+    `name` varchar(50) not null,
     foreign key(`user_id`) references `user`(`id`),
     primary key(`id`)
 );
@@ -26,4 +26,5 @@ create table `items` (
     primary key(`id`)
 );
 
-insert into user(`name`,`password`) values('测试用户','123456');
+insert into `user`(`name`,`password`) values('测试用户','123456');
+insert into `book`(`user_id`,`name`) values(1,'测试单词本');
